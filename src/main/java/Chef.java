c81f87f20302e81b1231e279923e16dcaac65bf5
Patrick <patrickcarvalho448@gmail.com>
2021-06-29 10:48:03 -0300

    Classe chef e testes

--- src/main/java/Chef.java
+++ src/main/java/Chef.java
@@ -1,2 +1,58 @@
-public class Chef {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Chef extends Funcionario{
+
+    private static float NOTA_MAXIMA = 10.0f;
+    private static float NOTA_MINIMA = 0.0f;
+
+    private float coeficienteBonus;
+    private List<Float> notas;
+    private float bonusMaximo;
+
+    public Chef(){
+        this.coeficienteBonus =0;
+        this.notas = new ArrayList<>();
+    }
+
+    public List<Float> getNotas() {
+        return notas;
+    }
+
+    public float getBonusMaximo() {
+        return bonusMaximo;
+    }
+
+    public void setBonusMaximo(float bonusMaximo) {
+        this.bonusMaximo = bonusMaximo;
+    }
+
+
+    public void adicionarNota(float nota){
+        if(nota >= NOTA_MINIMA && nota<= NOTA_MAXIMA){
+            notas.add(nota);
+        }
+    }
+
+    public float calcularBonus(){
+        float coeficienteBonus = calcularNotaMedia()/10;
+        return bonusMaximo*coeficienteBonus;
+    }
+
+    public float calcularNotaMedia(){
+        float somatorio =0;
+        float contador =0;
+
+        for(Float nota: notas){
+            somatorio+=nota;
+            contador++;
+        }
+
+        return somatorio/contador;
+    }
+
+    @Override
+    public float calcularSalario() {
+        return this.getSalarioBase() + calcularBonus();
+    }
 }
--- src/test/java/ChefTest.java
+++ src/test/java/ChefTest.java
@@ -1,21 +1,56 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ChefTest {
 
-    @Test
-    void adicionarNota() {
+    Chef chef;
+
+    @BeforeEach
+    void setup(){
+        chef = new Chef();
+        chef.setSalarioBase(1000);
+        chef.setBonusMaximo(1000);
     }
 
     @Test
-    void calcularBonus() {
+    void deveRetornarNotas() {
+        chef.adicionarNota(7.0f);
+        chef.adicionarNota(8.0f);
+        chef.adicionarNota(9.0f);
+        chef.adicionarNota(-0.1f);
+        chef.adicionarNota(10.1f);
+
+        assertEquals(Arrays.asList(7.0f, 8.0f, 9.0f), chef.getNotas());
     }
 
     @Test
-    void calcularNotaMedia() {
+    void deveRetornarMedia() {
+        chef.adicionarNota(7.0f);
+        chef.adicionarNota(8.0f);
+        chef.adicionarNota(9.0f);
+
+        assertEquals(8.0f, chef.calcularNotaMedia());
     }
 
     @Test
-    void calcularSalario() {
+    void deveRetornarBonusCalculado() {
+        chef.adicionarNota(7.0f);
+        chef.adicionarNota(8.0f);
+        chef.adicionarNota(9.0f);
+
+        assertEquals(800.0f, chef.calcularBonus());
+    }
+
+    @Test
+    void deveRetornarSalarioCalculado() {
+        chef.adicionarNota(7.0f);
+        chef.adicionarNota(8.0f);
+        chef.adicionarNota(9.0f);
+
+        assertEquals(1800.0f, chef.calcularSalario());
     }
 }
