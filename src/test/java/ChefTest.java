f2aa4d7d67859278ae4502627faae7313ade0fe2
Patrick <patrickcarvalho448@gmail.com>
2021-06-29 10:42:50 -0300

    Classe chef e testes

---
+++ src/test/java/ChefTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ChefTest {
+
+    @Test
+    void adicionarNota() {
+    }
+
+    @Test
+    void calcularBonus() {
+    }
+
+    @Test
+    void calcularNotaMedia() {
+    }
+
+    @Test
+    void calcularSalario() {
+    }
+}
